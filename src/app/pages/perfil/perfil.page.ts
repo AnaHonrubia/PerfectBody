bfe6d6ee5a199e578cb8a64f7045e76100e305ae
AnaHonrubia <honrubiaborjaa@gmail.com>
2026-04-29 14:11:19 +0200

    v1.2.0 - Integración del HttpClient para la API externa y persistencia de datos con el LocalStorage

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -3,6 +3,7 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { CalculadoraMetabolicaComponent } from '../../componentes/organismos/calculadora-metabolica/calculadora-metabolica.component';
 import { BotonOscuroComponent } from '../../componentes/atomos/boton-oscuro/boton-oscuro.component';
+import { FitAtomic } from 'src/app/services/fit-atomic';
 
 @Component({
   selector: 'app-perfil',
@@ -15,14 +16,22 @@ import { BotonOscuroComponent } from '../../componentes/atomos/boton-oscuro/boto
 export class PerfilPage {
   resultadoTMB: number | null = null;
 
-  manejarCalculo(datos: any) {
-    // Convertimos a número por si acaso vienen como string
-    const p = parseFloat(datos.peso);
-    const a = parseFloat(datos.altura);
-    const e = parseInt(datos.edad);
+  constructor(private fitService: FitAtomic) {
+    // Al cargar la página, intentamos recuperar datos guardados
+    const guardado = this.fitService.obtenerPerfil();
+    if (guardado) {
+      this.manejarCalculo(guardado);
+    }
+  }
 
-    // Fórmula para hombres (puedes ajustarla si quieres)
-    // TMB = 88.362 + (13.397 x peso) + (4.799 x altura) - (5.677 x edad)
+  manejarCalculo(datos: any) {
+    const p = Number(datos.peso);
+    const a = Number(datos.altura);
+    const e = Number(datos.edad);
+
     this.resultadoTMB = Math.round(88.362 + (13.397 * p) + (4.799 * a) - (5.677 * e));
+    
+    // GUARDAMOS EN LOCALSTORAGE
+    this.fitService.guardarPerfil(datos);
   }
 }
--- src/app/services/fit-atomic.ts
+++ src/app/services/fit-atomic.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class FitAtomic {
-  
+
+  private apiUrl = 'https://raw.githubusercontent.com/AnaHonrubia/APIs/refs/heads/main/alimentos.json';
+  private STORAGE_KEY = 'user_profile';
+
+  constructor(private http: HttpClient) { }
+
+  // 1. Obtener alimentos de GitHub
+  getAlimentos(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  // 2. Guardar datos en LocalStorage
+  guardarPerfil(datos: any) {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(datos));
+  }
+
+  // 3. Leer datos de LocalStorage
+  obtenerPerfil() {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data ? JSON.parse(data) : null;
+  }
 }
